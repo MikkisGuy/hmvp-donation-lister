72ded98b337bfd3343f51acaaa91114ca3b5cb24
MikkisGuy <69403498+MikkisGuy@users.noreply.github.com>
2021-03-13 15:22:09 +0200

    Now we can print undefined to the console, yay

--- src/DonationList/index.tsx
+++ src/DonationList/index.tsx
@@ -4,9 +4,9 @@ import fetch from 'node-fetch';
 
 function DonationList(): JSX.Element {
 
-  const donations = GetDonations('https://potti.mieli.fi/f/Donation/GetDonations/?collectionId=COL-16-3552&pageSize=50&startAt=0');
+  // const donations = iGetDonations('https://potti.mieli.fi/f/Donation/GetDonations/?collectionId=COL-16-3552&pageSize=50&startAt=0');
 
-  console.log(donations);
+  // console.log(donations);
 
   return <>
   
@@ -15,21 +15,50 @@ function DonationList(): JSX.Element {
 
 export default DonationList;
 
-interface donationsData  {
-  DonationId: string,
-  Name: string,
-  Amount: number,
-  Message: string
+class DonationsData{
+  name: string;
+  amount: number;
+  message: string;
+
+  constructor(name: string, amount: number, message: string){
+    this.name = name;
+    this.amount = amount;
+    this.message = message;
+  }
 }
 
-async function GetDonations(JsonUrl: string): Promise<donationsData[]>{
-
-  //  const don: donationsData = {
-  //    DonationId: '';
-  //  }
-
-  const response = await fetch(JsonUrl);
-  const data = await response.json();
-  
-  return data;
+// eslint-disable-next-line
+function FormatDonations(donation: any): DonationsData{
+  return {name: donation.name, amount: donation.amount, message: donation.message};
 }
+
+class DonationLister {
+  GetDonations(): Promise<DonationsData[]>{
+    return fetch('https://potti.mieli.fi/f/Donation/GetDonations/?collectionId=COL-16-3552&pageSize=50&startAt=0')
+      .then(res => res.json())
+      .then(res => res.map((donation: DonationsData[]) => FormatDonations(donation)));
+  }
+}
+
+const donationsApi = new DonationLister();
+
+donationsApi.GetDonations().then(donations => console.log(donations[0]['name']));
+
+
+// interface iDonationsData  {
+//   DonationId: string,
+//   Name: string,
+//   Amount: number,
+//   Message: string
+// }
+// 
+// async function iGetDonations(JsonUrl: string): Promise<iDonationsData>{
+// 
+//   const response = await fetch(JsonUrl);
+//   const data = await response.json();
+// 
+//   // const objs = JSON.parse(data) as MyObj[];
+// 
+//   return data;
+// }
+// 
