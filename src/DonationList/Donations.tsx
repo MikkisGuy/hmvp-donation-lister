a1e1434b0f65d65150613ed43498ff232a600ca8
MikkisGuy <69403498+MikkisGuy@users.noreply.github.com>
2021-03-23 22:18:16 +0200

    Development status after stream 23.3.2021

---
+++ src/DonationList/Donations.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import DonationsFetcher from './DonationsFetcher';
+
+// Siirretään pois täältä ettei kutsuta aina luokassa
+const donationsApi = new DonationsFetcher();
+const donationsList = donationsApi.GetDonations();
+
+export default class DonationComponents extends Component {
+  render(): React.ReactNode {
+    return donationsList.then((donations) => {
+      donations.map((donation, i) => {
+        return <Donation key={i} params={donation} />;
+      });
+    });
+  }
+}
+
+// Kopioitu: https://stackoverflow.com/questions/47740235/react-js-create-input-elements-from-json-schema
+// Muokataan omaan käyttöön
+class Donation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeValue = this.changeValue.bind(this);
+  }
+
+  render() {
+    return (
+      <div className={className}>
+        <label htmlFor={this.props.name}>{this.props.title}</label>
+        <input
+          onChange={this.changeValue}
+          name={this.props.name}
+          type={this.props.type}
+          value={this.props.value}
+        />
+      </div>
+    );
+  }
+}
