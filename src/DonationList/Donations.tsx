ed8c05937afdc54c4dc71cb1b9796e13caf4c951
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2021-05-11 21:45:14 +0300

    Changed to function components, started donations mapping

--- src/DonationList/Donations.tsx
+++ src/DonationList/Donations.tsx
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DonationsFetcher from './DonationsFetcher';
 
 // Siirretään pois täältä ettei kutsuta aina luokassa
 const donationsApi = new DonationsFetcher();
 const donationsList = donationsApi.GetDonations();
 
-export default class DonationComponents extends Component {
-  render(): React.ReactNode {
-    return donationsList.then((donations) => {
-      donations.map((donation, i) => {
-        return <Donation key={i} params={donation} />;
-      });
-    });
-  }
+export default function Donations(): JSX.Element {
+  console.log(donationsList);
+  return <div></div>;
 }
+
+// export default function DonationComponents() {
+//   render(): React.ReactNode {
+//     return donationsList.then((donations) => {
+//       donations.map((donation, i) => {
+//         return <Donation key={i} params={donation} />;
+//       });
+//     });
+//   }
+// }
 
 // Kopioitu: https://stackoverflow.com/questions/47740235/react-js-create-input-elements-from-json-schema
 // Muokataan omaan käyttöön
-class Donation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.changeValue = this.changeValue.bind(this);
-  }
+// class Donation extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.changeValue = this.changeValue.bind(this);
+//   }
 
-  render() {
-    return (
-      <div className={className}>
-        <label htmlFor={this.props.name}>{this.props.title}</label>
-        <input
-          onChange={this.changeValue}
-          name={this.props.name}
-          type={this.props.type}
-          value={this.props.value}
-        />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className={className}>
+//         <label htmlFor={this.props.name}>{this.props.title}</label>
+//         <input
+//           onChange={this.changeValue}
+//           name={this.props.name}
+//           type={this.props.type}
+//           value={this.props.value}
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/DonationList/DonationsFetcher.tsx
+++ src/DonationList/DonationsFetcher.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import fetch from 'node-fetch';
 
-class DonationsData {
+/* class DonationsData {
   Name: string;
   Amount: number;
   Message: string;
@@ -18,4 +19,28 @@ export default class DonationsFetcher {
       'https://potti.mieli.fi/f/Donation/GetDonations/?collectionId=COL-16-3552&pageSize=150&startAt=0'
     ).then((res) => res.json());
   }
+} */
+
+interface IDonationsData {
+  DonationId: string;
+  Name: string;
+  Amount: number;
+  Message: string;
+  TransactionDate: string;
+}
+export default function DonationsFetcher(): HTMLElement {
+  const [donations, setDonations] = useState<IDonationsData[]>([]);
+
+  async function fetchDonations(url: string) {
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+  const fetchDon = fetchDonations(
+    'https://potti.mieli.fi/f/Donation/GetDonations/?collectionId=COL-16-3552&pageSize=150&startAt=0'
+  );
+
+  const donationList = donations.map((donations) => <li>{donations.Name}</li>);
+
+  return donationList;
 }
